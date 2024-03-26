5073749ece27d2073169b31c866c03325edf6244
Roberto Vargas Castro <rvc.miigaik@gmail.com>
2024-03-26 06:42:26 -0600

    Se agrega instrucciones de la tarea

---
+++ anomalias_gravimetricas.R
@@ -0,0 +1,13 @@
+"
+Instrucciones metodologicas para la relizacion del trabajo de control No.1
+del curso:
+
+Teoria de las figuras planetarias y gravimetria.
+
+Tarea No.1: Calculo de anomalias gravitacionales.
+             Construccion de mapas gravimetricos.
+             
+1.1.- Calcule las anomalias al Aire Libre y las anomalias de Bouguer para los 
+primeros 10 puntos del catalogo, cuyo valores de anomalia no estan indicados
+en el catalogo. Precision del calculo 0.1mgal.
+"
