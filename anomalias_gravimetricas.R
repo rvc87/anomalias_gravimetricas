b75ad8b83e27dc8f8f64623e40aebb13b9021d1d
Roberto Vargas Castro <rvc.miigaik@gmail.com>
2024-03-26 19:01:58 -0600

    Se realizan calculos hasta determinar anomalia de aire libre y de bouguer

--- anomalias_gravimetricas.R
+++ anomalias_gravimetricas.R
@@ -29,3 +29,39 @@ gravedad_observada <- 979644.37
 
 latitud_decimal <- grados + (minutos / 60)
 latitud <- latitud_decimal * (pi / 180)
+
+# Calcular la gravedad normal sobre el elipsoide en la latitud de la 
+# estacion gravimetrica.
+
+a <- sin(latitud) ^ 2
+b <- sin(latitud * 2 ) ^ 2
+gravedad_normal_elipsoide <- 978030 * (1 + 0.005302 * a - 0.000007 * b)
+
+# Calcular la correccion al aire libre
+
+correccion_aire_libre <- -0.3086 * altitud
+
+# Calculo de la gravedad normal.
+
+gravedad_normal <- gravedad_normal_elipsoide + correccion_aire_libre
+
+# Calculo para la correcion de la capa intermedia
+
+correccion_capa_intermedia <- 0.0419 * 2.30 * altitud
+
+# Calculo de la anomalia al aire libre
+
+anomalia_aire_libre <- gravedad_observada - gravedad_normal
+
+# Calculo de anomalia de bouguer
+
+anomalia_bouguer <- anomalia_aire_libre - correccion_capa_intermedia
+
+
+
+
+
+
+
+
+
